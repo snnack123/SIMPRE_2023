cfae371e01d355680a32a6707c36ef1e1d8e8f75
Lungu Mihai Adrian <lungu.snk199@gmail.com>
2024-03-21 01:10:31 +0200

    deleted a log

--- pages/api/answer.js
+++ pages/api/answer.js
@@ -93,8 +93,6 @@ export default async function handler(req, res) {
 		type
 	} = req.body;
 
-	console.log('Incoming request: ', req.method, req.body);
-
 	if (!isAllowedMethod) {
 		return sendMethodNotAllowed()
 	}
