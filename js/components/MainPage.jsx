cea32a004b0be84430ff9abb1b2835dfe209ce12
Lungu Mihai Adrian <lungu.snk199@gmail.com>
2024-03-23 21:08:32 +0200

    added one more description

--- js/components/MainPage.jsx
+++ js/components/MainPage.jsx
@@ -41,6 +41,7 @@ export default function MainPage() {
 			<div className="container px-6 py-10 mx-auto">
 				<h1 className="mx-auto text-center text-6xl">Fun facts app</h1>
 				<p className="mx-auto text-center mt-4 text-3xl">This is an app that showcases fun facts</p>
+				<p className="mx-auto text-center mt-4 text-3xl">You can add, delete and view fun facts</p>
 
 				<div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
 					{records.map(record => (
